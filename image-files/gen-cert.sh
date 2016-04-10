200c6b9ff86beaa12b399febf1d957332d88994f
Oleg Stepura <github@oleg.stepura.com>
2016-04-10 23:55:55 +0200

    Append Intermediate Certificates

--- image-files/gen-cert.sh
+++ image-files/gen-cert.sh
@@ -21,6 +21,8 @@ if [ -n "$DOMAIN_ALT" ]; then
 	SAN_ARG="-reqexts SAN"
 fi
 
+mkdir -pv "$DIR_CERT"
+
 # Log output. http://stackoverflow.com/a/18462920
 # this would send stdout and stderr output into the log file, but would also leave you with fd 3 connected to the console
 exec 3>&1 1>>${LOG_FILE} 2>&1
@@ -33,15 +35,14 @@ fi
 
 # Run batch of command each after another's successful finish, else break
 echo "Generation of new certificate for '$DOMAIN' with alt '$DOMAIN_ALT' at $(date +'%Y-%m-%d %H:%M:%S')" && \
-mkdir -pv "$DIR_CERT" && \
 openssl genrsa -rand file:/dev/random 4096 > "$DIR_CERT/$DOMAIN.key" && \
 openssl req -new -sha256 -key "$DIR_CERT/$DOMAIN.key" -subj "/CN=$DOMAIN" $SAN_ARG -config <(echo -e "$CONFIG") > "$DIR_CERT/$DOMAIN.csr" && \
 python /usr/src/acme-tiny/acme_tiny.py --account-key "$ACCOUNT_KEY" --csr "$DIR_CERT/$DOMAIN.csr" --acme-dir "$WWW_DIR" > "$DIR_CERT/$DOMAIN.standalone.crt" && \
 wget -O - https://letsencrypt.org/certs/isrgrootx1.pem > "$DIR_CERT/letsencrypt-intermediate.pem" && \
-#wget -O - https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem >> "$DIR_CERT/letsencrypt-intermediate.pem" && \
-#wget -O - https://letsencrypt.org/certs/lets-encrypt-x2-cross-signed.pem >> "$DIR_CERT/letsencrypt-intermediate.pem" && \
-#wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem >> "$DIR_CERT/letsencrypt-intermediate.pem" && \
-#wget -O - https://letsencrypt.org/certs/lets-encrypt-x4-cross-signed.pem >> "$DIR_CERT/letsencrypt-intermediate.pem" && \
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem >> "$DIR_CERT/letsencrypt-intermediate.pem" && \
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x2-cross-signed.pem >> "$DIR_CERT/letsencrypt-intermediate.pem" && \
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x3-cross-signed.pem >> "$DIR_CERT/letsencrypt-intermediate.pem" && \
+wget -O - https://letsencrypt.org/certs/lets-encrypt-x4-cross-signed.pem >> "$DIR_CERT/letsencrypt-intermediate.pem" && \
 cat "$DIR_CERT/$DOMAIN.standalone.crt" "$DIR_CERT/letsencrypt-intermediate.pem" > "$DIR_CERT/$DOMAIN.crt"
 
 # Check if batch of commands has run successfully
